f51e979140b19a705a029da54ab4ab3173679573
nikhilmotivitylabs <106816016+nikhilmotivitylabs@users.noreply.github.com>
2022-07-26 23:25:18 +0530

    Add files via upload

--- Superdata/CollegeProfesser.java
+++ Superdata/CollegeProfesser.java
@@ -5,6 +5,26 @@ public class CollegeProfesser {
     void welcome(){
         System.out.println("welcome to college");
     }
+    int id;
+    String name;
+
+    String course;
+
+    CollegeProfesser(){}
+
+    CollegeProfesser(int id,String name){
+        System.out.println("hi");
+        this.id=id;
+        this.name=name;
+    }
+
+    CollegeProfesser(int id,String name,String course){
+        this(id,name);
+        this.id=id;
+        this.name=name;
+    }
+
+
 
     void greeting(){
         System.out.println("good morinring");
@@ -15,5 +35,8 @@ public class CollegeProfesser {
     public static void main(String[] args) {
         CollegeProfesser data=new CollegeProfesser();
         data.greeting();
+
+        CollegeProfesser data1=new CollegeProfesser(101,"nikki","react");
+        
     }
 }
